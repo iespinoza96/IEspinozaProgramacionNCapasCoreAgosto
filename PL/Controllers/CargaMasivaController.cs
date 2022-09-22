3aa1979f7376d9a9e1a58f412733c879d1ee88da
Leoesbravo <lescogidobravo@outlook.com>
2022-09-22 12:20:59 -0500

    appsettings

---
+++ PL/Controllers/CargaMasivaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class CargaMasivaController : Controller
+    {
+        //private readonly IConfiguration _configuration;
+
+        //private readonly IHostingEnvironment _hostingEnvironment;
+
+        //public CargaMasiva(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        //{
+        //    _configuration = configuration;
+        //    _hostingEnvironment = hostingEnvironment;
+        //}
+        private readonly IConfiguration _configuration;
+
+
+        public CargaMasivaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public ActionResult CargaMasiva()
+        {
+            return View();
+        }
+    }
+}
