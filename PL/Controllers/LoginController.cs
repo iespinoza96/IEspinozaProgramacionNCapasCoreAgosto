01c2302a5f043a7c4665ccdb898ad5bdc2376b66
Leoesbravo <lescogidobravo@outlook.com>
2022-10-06 17:26:59 -0500

    Login

---
+++ PL/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
